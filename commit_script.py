9e54e8113968f004d338effab69067a119c2477b
kamilkaczmarek97 <kamil.kaczmarek.us@gmail.com>
2024-01-04 13:32:37 +0100

    Added `commit_script.py`

---
+++ commit_script.py
@@ -0,0 +1,14 @@
+import subprocess
+import datetime
+import time
+
+def commit_and_push():
+    repo_dir = 'C:\\Users\\clean\\Dud'
+    subprocess.call(['git', '-C', repo_dir, 'commit', '--allow-empty', '-m', f'Empty commit on {datetime.datetime.now()}'])
+    subprocess.call(['git', '-C', repo_dir, 'push'])
+
+if __name__ == "__main__":
+    while True:
+        commit_and_push()
+        print("Commit done. Next commit in 24 hours.")
+        time.sleep(300)  # Sleep for 24 hours
